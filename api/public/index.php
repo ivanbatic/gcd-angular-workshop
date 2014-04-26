624f23d392298528a3ea9a5dea8a8e9f1098a74e
Ivan Batić <ivan.batic@live.com>
2014-04-26 23:41:12 +0200

    Added cors options interception

--- api/public/index.php
+++ api/public/index.php
@@ -1,4 +1,16 @@
 <?php
+
+// respond to preflights
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    // return only the headers and not the content
+    // only allow CORS if we're doing a GET - i.e. no saving for now.
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'GET') {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Headers: X-Requested-With');
+    }
+    exit;
+}
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
